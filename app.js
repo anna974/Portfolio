33223b8ffc887dce8a1b4d7e94b9c3be8b94e51d
anna974 <annapiaraly@gmail.com>
2018-06-08 15:34:58 +0400

    ajout menu collapse

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-    $(".partie").click(function () {
-        $(".icones").fadeToggle();
+    
+    $("#apropos").click(function(){
+        $("#div1").fadeToggle("slow");  
+    }); 
+
+    $("#competences").click(function(){
+        $("#div2").show("slow");  
+    }); 
+
+    $("#projets").click(function(){
+        $("#div3").show("slow");  
+    }); 
+
+    $("#contact").click(function(){
+        $("#div4").show("slow");  
     }); 
 })
